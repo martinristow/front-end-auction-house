89578c1b051d3766159488c161c06a813a047eab
martinristow <martinristov084@gmail.com>
2025-01-24 01:06:08 +0100

    Added Footer

---
+++ auction-house/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+import "./Footer.css";
+
+export default function App() {
+    return (
+        <MDBFooter bgColor='light' className='text-center text-lg-left pozicija'>
+            <div className='text-center p-3' style={{ backgroundColor: "#0083ca" }}>
+                &copy; {new Date().getFullYear()} Copyright:{' '}
+                Auction House Page
+            </div>
+        </MDBFooter>
+    );
+}
