69f457ee368e149aaf37b2c0f3ad617d3d1b30e1
martinristow <martinristov084@gmail.com>
2025-01-24 15:45:38 +0100

    Added and Fixed logic at private routes

--- auction-house/src/App.jsx
+++ auction-house/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
@@ -5,7 +6,10 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./Components/Navigation.jsx";
-
+import FinishedAuctions from "./Pages/FinishedAuctions.jsx";
+import ActiveAuctions from "./Pages/ActiveAuctions.jsx";
+import GetAuctionById from "./Pages/GetAuctionById.jsx";
+import PrivateRoute from './Components/PrivateRoute';
 
 function App() {
     return (
@@ -15,6 +19,11 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/categories/closed-auctions" element={<FinishedAuctions />} />
+                <Route path="/categories/active-auctions" element={<ActiveAuctions />} />
+
+                {/* Use PrivateRoute to wrap protected route */}
+                <Route path="/auction/:id" element={<PrivateRoute><GetAuctionById /></PrivateRoute>} />
             </Routes>
         </Router>
     );
