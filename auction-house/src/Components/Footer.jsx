103ca690786fa4cb96472dfc97a85a244c1a2260
martinristow <martinristov084@gmail.com>
2025-01-24 19:01:59 +0100

    Little Change on text

--- auction-house/src/Components/Footer.jsx
+++ auction-house/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <MDBFooter bgColor='light' className='text-center text-lg-left pozicija'>
             <div className='text-center p-3' style={{ backgroundColor: "#0083ca" }}>
-                &copy; {new Date().getFullYear()} Авторски права:{' '}
+                &copy; {new Date().getFullYear()}{' '}
                 Аукциска Куќа. Сите права задржани.
             </div>
         </MDBFooter>
