67fe17c14b4c916fb91a1f7832ce5a8fea6756ae
martinristow <martinristov084@gmail.com>
2025-01-25 19:36:56 +0100

    Added Footer Component

--- auction-house/src/Pages/FinishedAuctions.jsx
+++ auction-house/src/Pages/FinishedAuctions.jsx
@@ -1,11 +1,13 @@
 // src/pages/FinishedAuctionPage.js
 import React from "react";
 import ActiveAuctions from "../components/FinishedAuctions.jsx";
+import Footer from "../components/Footer.jsx";
 
 const FinishedAuctionPage = () => {
     return (
         <div>
             <ActiveAuctions />
+            <Footer />
         </div>
     );
 };
