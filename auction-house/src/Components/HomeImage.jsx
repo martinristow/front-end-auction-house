e27289c74bdccb72194f95abf714c714ab748c16
martinristow <martinristov084@gmail.com>
2025-01-26 16:59:27 +0100

    Added a link to the button to redirect to /categories/all

--- auction-house/src/Components/HomeImage.jsx
+++ auction-house/src/Components/HomeImage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./HomeImage.css";
 import Slika from "./../assets/slika.png";
-
+import { Link } from "react-router-dom";
 function HomeImage() {
     return (
+    <>
         <div className="full-screen-container">
             <img src={Slika} alt="Home" className="full-screen-image" />
             <div className="image-overlay">
                 <h1 className="patot-do-sovrsenata-ponuda">Патот до совршена понуда!</h1>
                 <p className="tekst">Создадете, следете и освојувајте аукции со леснотија и сигурност. Нашата платформа ви овозможува едноставен пристап до различни аукции, каде што можете да ги следите вашите понуди во реално време и да бидете уверени дека секој чекор е безбеден и сигурен.</p>
-                <button className="cta-button">Започнете сега</button>
+                <Link to="/categories/all">
+                    <button className="cta-button">Започнете сега</button>
+                </Link>
             </div>
         </div>
+    </>
     );
 }
 
