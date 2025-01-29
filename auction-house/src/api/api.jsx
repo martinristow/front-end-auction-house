d8ea20f67b7f9c306dc20456c590867910d0b7dd
martinristow <martinristov084@gmail.com>
2025-01-29 13:15:06 +0100

    Added comments

--- auction-house/src/api/api.jsx
+++ auction-house/src/api/api.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const API = axios.create({
-    baseURL: "http://localhost:8000", // nasata localhost ruta
+const API = axios.create({ // kreirame instanca na axios so baseURL: localhost:8000
+    baseURL: "http://localhost:8000", // site baranja napraveni preku API ke se prakjaat do lokalniot server na porta 8000
 });
 
 // Dodavanje na Bearer Token
 API.interceptors.request.use((req) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem("token"); // pred da se isprati sekoe baranje go proveruvam localStorage za token
+    if (token) { // dokolku ima zacuvan token go dodadame zaglavieto na baranjeto kako Bearer Token
+        req.headers.Authorization = `Bearer ${token}`; // vazno e za avtentikacija, t.e serverot kje znae koj korisnik isprakja baranje
     }
     return req;
 });
@@ -17,11 +17,11 @@ API.interceptors.request.use((req) => {
 API.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error.response && error.response.status === 401) {
+        if (error.response && error.response.status === 401) { // ako serverot vrati error response se proveruva error response status i ako e 401 se brise tokenot
             localStorage.removeItem("token");
-            window.location = "/login";
+            window.location = "/login"; // go prenasocuvame na login rutata
         }
-        return Promise.reject(error);
+        return Promise.reject(error); // znaci deka funkcijata sto go povikala baranjeto ke mora da ja obraboti greskata
     }
 );
 
